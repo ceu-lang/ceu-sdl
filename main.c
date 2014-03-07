a56d224f7abfa193e1031b300a837e8b188c32f2
Francisco Sant'Anna <francisco.santanna@gmail.com>
2014-03-07 16:26:04 -0300

    (*) update to CEU_OS branch     (^) ceu_out_wclock => ceu_out_wclock_set     (^) external memory (CEU_DATA)     (^) tests for CEU_RET     (+) ceu_types.h (-) bug: WCLOCK_nxt loop should emit 0us

--- main.c
+++ main.c
@@ -28,11 +28,9 @@
 
 #include "_ceu_app.h"
 
-int ret_val;
-
 s32 WCLOCK_nxt;
 #ifndef CEU_IN_SDL_DT
-#define ceu_out_wclock(us) WCLOCK_nxt = us;
+#define ceu_out_wclock_set(us) WCLOCK_nxt = us;
 #endif
 
 #ifdef CEU_ASYNCS
@@ -74,17 +72,31 @@ int main (int argc, char *argv[])
     CEU_THREADS_MUTEX_LOCK(&CEU.threads_mutex);
 #endif
 
-    if (ceu_go_init(&ret_val))
-        goto END;
+    char CEU_DATA[sizeof(CEU_Main)];
+    tceu_app app;
+        app.data = (tceu_org*) &CEU_DATA;
+        app.init = &ceu_app_init;
 
-// TODO: push START into queue
-#ifdef CEU_IN_START
-    if (ceu_go_event(&ret_val, CEU_IN_START, NULL))
+    app.init(&app);    /* calls CEU_THREADS_MUTEX_LOCK() */
+#ifdef CEU_RET
+    if (! app.isAlive)
         goto END;
 #endif
-#ifdef CEU_IN_SDL_REDRAW
-    if (ceu_go_event(&ret_val, CEU_IN_SDL_REDRAW, NULL))
+
+#ifdef CEU_IN_OS_START
+    ceu_sys_go(&app, CEU_IN_OS_START, (tceu_evtp)NULL);
+#ifdef CEU_RET
+    if (! app.isAlive)
         goto END;
+#endif
+#endif
+
+#ifdef CEU_IN_SDL_REDRAW
+    ceu_sys_go(&app, CEU_IN_SDL_REDRAW, (tceu_evtp)NULL);
+#ifdef CEU_RET
+    if (! app.isAlive)
+        goto END;
+#endif
 #endif
 
     SDL_Event evt;
@@ -156,19 +168,29 @@ int main (int argc, char *argv[])
             {
                 redraw = WCLOCK_nxt <= 1000*dt;
 #endif
-                if (ceu_go_wclock(&ret_val, 1000*dt))
+                ceu_sys_go(&app, CEU_IN__WCLOCK, (tceu_evtp)(1000*dt));
+#ifdef CEU_RET
+                if (! app.isAlive)
                     goto END;
+#endif
+
                 while (WCLOCK_nxt <= 0) {
-                    if (ceu_go_wclock(&ret_val, 0))
+                    ceu_sys_go(&app, CEU_IN__WCLOCK, (tceu_evtp)0);
+#ifdef CEU_RET
+                    if (! app.isAlive)
                         goto END;
+#endif
                 }
 #ifndef CEU_IN_SDL_DT
             }
 #endif
 #endif
 #ifdef CEU_IN_SDL_DT
-            if (ceu_go_event(&ret_val, CEU_IN_SDL_DT, (void*)dt))
+            ceu_sys_go(&app, CEU_IN_SDL_DT, (tceu_evtp)dt);
+#ifdef CEU_RET
+            if (! app.isAlive)
                 goto END;
+#endif
             redraw = 1;
 #endif
         }
@@ -177,17 +199,62 @@ int main (int argc, char *argv[])
         if (has)
         {
             int handled = 1;        // =1 for defined events
+            tceu_evtp evtp = (tceu_evtp)(void*)&evt;
 //printf("EVT: %x\n", evt.type);
-            int ret;
             switch (evt.type) {
 #ifdef CEU_IN_SDL_QUIT
                 case SDL_QUIT:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_QUIT, NULL);
+                    ceu_sys_go(&app, CEU_IN_SDL_QUIT, evtp);
                     break;
 #endif
 #ifdef CEU_IN_SDL_WINDOWEVENT
                 case SDL_WINDOWEVENT:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_WINDOWEVENT, &evt);
+                    ceu_sys_go(&app, CEU_IN_SDL_WINDOWEVENT, evtp);
+                    break;
+#endif
+#ifdef CEU_IN_SDL_KEYDOWN
+                case SDL_KEYDOWN:
+                    ceu_sys_go(&app, CEU_IN_SDL_KEYDOWN, evtp);
+                    break;
+#endif
+#ifdef CEU_IN_SDL_KEYUP
+                case SDL_KEYUP:
+                    ceu_sys_go(&app, CEU_IN_SDL_KEYUP, evtp);
+                    break;
+#endif
+#ifdef CEU_IN_SDL_TEXTINPUT
+                case SDL_TEXTINPUT:
+                    ceu_sys_go(&app, CEU_IN_SDL_TEXTINPUT, evtp);
+                    break;
+#endif
+#ifdef CEU_IN_SDL_TEXTEDITING
+                case SDL_TEXTEDITING:
+                    ceu_sys_go(&app, CEU_IN_SDL_TEXTEDITING, evtp);
+                    break;
+#endif
+#ifdef CEU_IN_SDL_MOUSEMOTION
+                case SDL_MOUSEMOTION:
+                    ceu_sys_go(&app, CEU_IN_SDL_MOUSEMOTION, evtp);
+                    break;
+#endif
+#ifdef CEU_IN_SDL_MOUSEBUTTONDOWN
+                case SDL_MOUSEBUTTONDOWN:
+                    ceu_sys_go(&app, CEU_IN_SDL_MOUSEBUTTONDOWN, evtp);
+                    break;
+#endif
+#ifdef CEU_IN_SDL_MOUSEBUTTONUP
+                case SDL_MOUSEBUTTONUP:
+                    ceu_sys_go(&app, CEU_IN_SDL_MOUSEBUTTONUP, evtp);
+                    break;
+#endif
+#ifdef CEU_IN_SDL_FINGERDOWN
+                case SDL_FINGERDOWN:
+                    ceu_sys_go(&app, CEU_IN_SDL_FINGERDOWN, evtp);
+                    break;
+#endif
+#ifdef CEU_IN_SDL_FINGERUP
+                case SDL_FINGERUP:
+                    ceu_sys_go(&app, CEU_IN_SDL_FINGERUP, evtp);
                     break;
 #endif
 
@@ -198,7 +265,7 @@ int main (int argc, char *argv[])
                     isPaused = 1;
 #endif
 #ifdef CEU_IN_SDL_APP_WILLENTERBACKGROUND
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_APP_WILLENTERBACKGROUND, &evt);
+                    ceu_sys_go(&app, CEU_IN_SDL_APP_WILLENTERBACKGROUND, evtp);
 #endif
                     break;
 #endif
@@ -210,58 +277,13 @@ int main (int argc, char *argv[])
                     old = SDL_GetTicks();   // ignores previous 'old' on resume
 #endif
 #ifdef CEU_IN_SDL_APP_WILLENTERFOREGROUND
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_APP_WILLENTERFOREGROUND, &evt);
+                    ceu_sys_go(&app, CEU_IN_SDL_APP_WILLENTERFOREGROUND, evtp);
 #endif
-                    break;
-#endif
-#ifdef CEU_IN_SDL_KEYDOWN
-                case SDL_KEYDOWN:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_KEYDOWN, &evt);
-                    break;
-#endif
-#ifdef CEU_IN_SDL_KEYUP
-                case SDL_KEYUP:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_KEYUP, &evt);
-                    break;
-#endif
-#ifdef CEU_IN_SDL_TEXTINPUT
-                case SDL_TEXTINPUT:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_TEXTINPUT, &evt);
-                    break;
-#endif
-#ifdef CEU_IN_SDL_TEXTEDITING
-                case SDL_TEXTEDITING:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_TEXTEDITING, &evt);
-                    break;
-#endif
-#ifdef CEU_IN_SDL_MOUSEMOTION
-                case SDL_MOUSEMOTION:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_MOUSEMOTION, &evt);
-                    break;
-#endif
-#ifdef CEU_IN_SDL_MOUSEBUTTONDOWN
-                case SDL_MOUSEBUTTONDOWN:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_MOUSEBUTTONDOWN, &evt);
-                    break;
-#endif
-#ifdef CEU_IN_SDL_MOUSEBUTTONUP
-                case SDL_MOUSEBUTTONUP:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_MOUSEBUTTONUP, &evt);
-                    break;
-#endif
-#ifdef CEU_IN_SDL_FINGERDOWN
-                case SDL_FINGERDOWN:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_FINGERDOWN, &evt);
-                    break;
-#endif
-#ifdef CEU_IN_SDL_FINGERUP
-                case SDL_FINGERUP:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_FINGERUP, &evt);
                     break;
 #endif
 #ifdef CEU_IN_SDL_FINGERMOTION
                 case SDL_FINGERMOTION:
-                    ret = ceu_go_event(&ret_val, CEU_IN_SDL_FINGERMOTION, &evt);
+                    ceu_sys_go(&app, CEU_IN_SDL_FINGERMOTION, evtp);
 #ifdef SDL_MOTION_FLOOD_AVOID
                     // handle MOTION floods
                     SDL_FlushEventsFilter(FLOOD_FILTER,
@@ -273,14 +295,19 @@ int main (int argc, char *argv[])
                 default:
                     handled = 0;    // undefined event
             }
-            if (ret) goto END;
+#ifdef CEU_RET
+            if (! app.isAlive) goto END;
+#endif
             redraw = redraw || handled;
         }
 
 #ifdef CEU_IN_SDL_REDRAW
         if (redraw) {
-            if (ceu_go_event(&ret_val, CEU_IN_SDL_REDRAW, NULL))
+            ceu_sys_go(&app, CEU_IN_SDL_REDRAW, (tceu_evtp)NULL);
+#ifdef CEU_RET
+            if (! app.isAlive)
                 goto END;
+#endif
         }
 #endif
 
@@ -288,8 +315,11 @@ int main (int argc, char *argv[])
 
 #ifdef CEU_ASYNCS
         if (ASYNC_nxt) {
-            if (ceu_go_async(&ret_val, NULL))
+            ceu_sys_go(&app, CEU_IN__ASYNC, (tceu_evtp)NULL);
+#ifdef CEU_RET
+            if (! app.isAlive)
                 goto END;
+#endif
         }
 #endif
     }
@@ -299,6 +329,10 @@ END:
     CEU_THREADS_MUTEX_UNLOCK(&CEU.threads_mutex);
 #endif
     SDL_Quit();         // TODO: slow
-    return ret_val;
+#ifdef CEU_RET
+    return app.ret;
+#else
+    return 0;
+#endif
 }
 
